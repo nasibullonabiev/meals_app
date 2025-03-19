199faa7293e05f6839fa4d97d92ae24a6ced9eb6
Nasibullo Nabiev <105308913+nasibullonabiev@users.noreply.github.com>
2025-03-20 00:11:44 +0100

    Added Some Animations

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -7,6 +7,7 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
+
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
       return false;
